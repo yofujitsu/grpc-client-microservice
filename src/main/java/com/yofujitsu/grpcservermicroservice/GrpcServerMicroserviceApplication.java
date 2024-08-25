31c07c85aacd0d0fbf655b34051dc797c3cb8926
yofujitsu <yofujitsuuu@gmail.com>
2024-08-25 11:37:05 +0300

    first commit

---
+++ src/main/java/com/yofujitsu/grpcservermicroservice/GrpcServerMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.yofujitsu.grpcservermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcServerMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcServerMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yofujitsu/grpcservermicroservice/GrpcServerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yofujitsu.grpcservermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcServerMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
